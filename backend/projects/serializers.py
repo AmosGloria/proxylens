d6dbce25e736eebf37ae91dbd54f83d7d811d085
Amos Gloria <amoskesegloria@gmail.com>
2026-06-27 01:23:53 +0100

    feat: set up projects API

--- backend/projects/serializers.py
+++ backend/projects/serializers.py
@@ -1,2 +1,55 @@
 from rest_framework import serializers
-from .models import projects, TargetURL
+from .models import Project, TargetURL
+
+class TargetURLSerializer(serializers.ModelSerializer):
+    project_name = serializers.CharField(source="project.name", read_only=True)
+    
+    class Meta:
+        model = TargetURL
+        fields = [
+            "id",
+            "project",
+            "project_name",
+            "url",
+            "label",
+            "is_active",
+            "last_checked_at",
+            "next_check_at",
+            "created_at",
+        ]
+        read_only_fields = [
+            "id",
+            "project_name",
+            "last_checked_at",
+            "next_check_at",
+            "created_at",
+        ]
+        
+class ProjectSerializer(serializers.ModelSerializer):
+    owner_username = serializers.CharField(source="owner.username", read_only=True)
+    target_urls_count = serializers.SerializerMethodField()
+    
+    class Meta :
+        model = Project
+        fields = [
+            "id",
+            "owner",
+            "owner_username",
+            "name",
+            "domain",
+            "description",
+            "is_active",
+            "target_urls_count",
+            "created_at",
+            "updated_at",
+        ]   
+        read_only_fields = [
+            "id",
+            "owner",
+            "owner_username",
+            "target_urls_count",
+            "created_at",
+            "updated_at",
+        ] 
+    def get_target_urls_count(self, obj):
+        return obj.target_urls.count() 
--- backend/projects/views.py
+++ backend/projects/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from rest_framework.exceptions import PermissionDenied
 
-# Create your views here.
+from .models import Project, TargetURL
+from .serializers import ProjectSerializer, TargetURLSerializer
+
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    serializer_class = ProjectSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Project.objects.filter(owner=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class TargetURLViewSet(viewsets.ModelViewSet):
+    serializer_class = TargetURLSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return TargetURL.objects.filter(project__owner=self.request.user)
+
+    def perform_create(self, serializer):
+        project = serializer.validated_data.get("project")
+
+        if project.owner != self.request.user:
+            raise PermissionDenied("You do not have permission to add URLs to this project.")
+
+        serializer.save()
+
+    def perform_update(self, serializer):
+        project = serializer.validated_data.get("project", serializer.instance.project)
+
+        if project.owner != self.request.user:
+            raise PermissionDenied("You do not have permission to update this URL.")
+
+        serializer.save()
